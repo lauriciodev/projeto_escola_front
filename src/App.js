b1249d1bf7d444746eb2245a77f53758f3691944
lauriciodev <developerwebmasterpro@gmail.com>
2023-05-02 00:38:15 -0300

    global style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Login from "./pages/login/login";
+import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <GlobalStyles />
     </div>
   );
 }
